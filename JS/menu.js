d8c195ead1e4b0b4cc854a0355fcdcd2d7ce7b6f
Rassas Amine <rassas.ahmed.amine@gmail.com>
2017-06-12 15:36:51 +0200

    All source files

---
+++ JS/menu.js
@@ -0,0 +1,12 @@
+function navselect(x){
+  $(".is-active").removeClass("is-active");
+  if (x == p1){
+    $("#p1").addClass("is-active");
+  }
+  if (x == p2){
+    $("#p2").addClass("is-active");
+  }
+  if (x == p3){
+    $("#p3").addClass("is-active");
+  }
+};
